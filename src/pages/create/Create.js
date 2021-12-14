847d25008649f849d9b73f0b867046d3b076eea9
Liubou Grebennikova <liubou.grebennikova@gmail.com>
2021-12-14 13:18:19 +0200

    create new project form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,14 +1,106 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Select from 'react-select'
+import { useCollection } from '../../hooks/useCollection'
 
 // styles
 import './Create.css'
 
-export default function Create() {
+const categories = [
+  { value: 'development', label: 'Development' },
+  { value: 'design', label: 'Design' },
+  { value: 'sales', label: 'Sales' },
+  { value: 'marketing', label: 'Marketing' },
+]
 
+export default function Create() {
+  const { documents } = useCollection('users')
+  const [users, setUsers] = useState([])
+
+  // form field values
+  const [name, setName] = useState('')
+  const [details, setDetails] = useState('')
+  const [dueDate, setDueDate] = useState('')
+  const [category, setCategory] = useState('')
+  const [assignedUsers, setAssignedUsers] = useState([])
+  const [formError, setFormError] = useState(null)
+
+  useEffect(() => {
+    if(documents) {
+      const options = documents.map(user => {
+        return {
+          value: user,
+          label: user.displayName
+        }
+      })
+      setUsers(options)
+    }
+
+  }, [documents])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setFormError(null)
+
+    if(!category) {
+      setFormError('Please select category')
+      return
+    }
+    if(assignedUsers.length < 1) {
+      setFormError('Please assign the project to at least 1 user')
+      return
+    }
+    console.log( name, details, dueDate, category.value, assignedUsers)
+  }
 
   return (
-    <div>
-      create
+    <div className="create-form">
+      <h2 className="page-title">Create a new project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Project name:</span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+        <label>
+          <span>Project datails:</span>
+          <textarea
+            required
+            type="text"
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          />
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input
+            required
+            type="date"
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
+        <label>
+          <span>Project category:</span>
+          <Select
+            onChange={(option) => setCategory(option)}
+            options={categories}
+          />
+        </label>
+        <label>
+          <span>Assign to:</span>
+          <Select
+            onChange={(option) => setAssignedUsers(option)}
+            options={users}
+            isMulti
+          />
+        </label>
+        <button className="btn">Add Project</button>
+        {formError && <p className="error">{formError}</p>}
+      </form>
     </div>
   )
 }
