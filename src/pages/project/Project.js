bd2901791264ad804909cfdeb0a9721e0d489b5b
Liubou Grebennikova <liubou.grebennikova@gmail.com>
2021-12-22 09:47:18 +0200

    make a comments form

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -3,6 +3,7 @@ import { useDocument } from '../../hooks/useDocument'
 
 // styles & components
 import './Project.css'
+import ProjectComments from './ProjectComments'
 import ProjectSummary from './ProjectSummary'
 
 export default function Project() {
@@ -18,6 +19,7 @@ export default function Project() {
   return (
     <div className="project-details">
       <ProjectSummary project={document} />
+      <ProjectComments />
     </div>
   )
 }
